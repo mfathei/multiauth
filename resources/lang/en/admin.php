8e6762cd6c2f104a1fe6a61c20a88e831390f8aa
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-09-23 14:21:19 +0200

    make admin login and name routes

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'AdminLogin' => 'ADMIN Login',
+];
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,4 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'AdminController@index')->name('admin');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+});
